6ba53ca6656e1c6487596286a728eff5ff18fb00
mastoffel <martin.adam.stoffel@gmail.com>
2024-06-10 17:14:29 +0100

    add first draft for attention forward pass

--- attention/src/attention.rs
+++ attention/src/attention.rs
@@ -1,7 +1,7 @@
-use std::marker::PhantomData;
-
+use anyhow::Error;
 use interfaces::deep_learning::{DLModule, LinearLayer};
 use interfaces::tensors::{Element, Tensor};
+use std::marker::PhantomData;
 
 pub trait MaskedSelfAttention<T, E>: DLModule<T, E>
 where
@@ -24,6 +24,7 @@ where
     pub query_weights: L,
     pub key_weights: L,
     pub value_weights: L,
+    pub mask: T,
     pub _marker_t: PhantomData<T>,
     pub _marker_e: PhantomData<E>,
 }
@@ -34,8 +35,13 @@ where
     T: Tensor<E>,
     E: Element,
 {
-    pub fn new() -> Self {
+    pub fn new(embedding_dims: (usize, usize)) -> Self {
         // Generate weights tensors W_Q, W_K, W_V with shapes like X (embedding matrix)
+
+        // Assume T has shape: (batch x sequence x channel)
+        // X.size
+        // let W_Q = L::new();
+        // let W_K = L::new();
         todo!()
     }
 }
@@ -50,7 +56,22 @@ where
     type DLModuleError = <T as Tensor<E>>::TensorError;
 
     fn forward(&self, x: &T) -> Result<T, Self::DLModuleError> {
-        todo!()
+        // todo!()
+        // let masked_x: T = self.mask.forward(x)?;
+        let masked_x: T = self.mask * x.clone(); // element-wise multiplication
+
+        let query = self.query_weights.forward(x).unwrap(); // just a matmul, Unwrap used since we currently do not have conversion implemented
+        let key: T = self.key_weights.forward(x).unwrap();
+        let value: T = self.value_weights.forward(x).unwrap();
+        let last_dim_of_keys = key.shape().last().unwrap();
+        // let last_dim_of_keys = key.shape().last().ok_or(anyhow!("Empty dim"))?;
+        // let att: T = query.matmul(&key.transpose()) * 1 / sqrtf64(last_dim_of_keys)?; // make sure only last two dimensions are transposed
+        // let att: T = query.matmul(&key.transpose()).unwrap() * 1. / E::sqrt(last_dim_of_keys);
+        let att: T = query.matmul(&key.transpose()).unwrap() * T::from_f64(1.)
+            / T::from_usize(*last_dim_of_keys);
+        // matmul with V
+        let att_v: T = att.matmul(&value);
+        // make sure only last two dimensions are transposed
     }
 
     fn params(&self) -> Vec<E> {
