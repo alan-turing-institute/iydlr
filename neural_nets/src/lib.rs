ce6cc2b01ad701bb5772d2694296283ec025656b
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-06-10 16:44:05 +0100

    start building linear layer
    Co-authored-by: Ed Chapman <edchapman88@users.noreply.github.com>

--- neural_nets/src/lib.rs
+++ neural_nets/src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use anyhow;
+use interfaces::deep_learning::{DLModule, LinearLayer};
+use interfaces::tensors::Element;
+use interfaces::tensors::Tensor;
+use std::marker::PhantomData;
+
+struct LinLayer<T: Tensor<E>, E: Element> {
+    w: T,
+    b: T,
+    tensor_element_phantom: PhantomData<E>,
+}
+
+impl<T, E> DLModule<T, E> for LinLayer<T, E>
+where
+    T: Tensor<E>,
+    E: Element,
+{
+    type DLModuleError = <T as Tensor<E>>::TensorError;
+    fn forward(&self, x: &T) -> Result<T, Self::DLModuleError> {
+        todo!()
+    }
+    fn params(&self) -> Vec<E> {
+        todo!()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // #[test]
+    // fn it_works() {
+    //     let result = add(2, 2);
+    //     assert_eq!(result, 4);
+    // }
 }
