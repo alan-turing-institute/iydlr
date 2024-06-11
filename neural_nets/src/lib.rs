9da64ccfbafe254999c2eeda238887c3544855fb
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-06-11 15:01:45 +0100

    working on LinLayer implementation

--- neural_nets/src/lib.rs
+++ neural_nets/src/lib.rs
@@ -1,8 +1,8 @@
-use anyhow;
 use interfaces::deep_learning::{DLModule, LinearLayer};
 use interfaces::tensors::Element;
 use interfaces::tensors::Tensor;
 use std::marker::PhantomData;
+pub mod embedding_table;
 
 struct LinLayer<T: Tensor<E>, E: Element> {
     w: T,
@@ -17,10 +17,38 @@ where
 {
     type DLModuleError = <T as Tensor<E>>::TensorError;
     fn forward(&self, x: &T) -> Result<T, Self::DLModuleError> {
-        todo!()
+        Ok(self.w.matmul(&x.clone().transpose())? + self.b.clone())
     }
     fn params(&self) -> Vec<E> {
-        todo!()
+        let mut res: Vec<E> = self.w.clone().into();
+        res.extend(self.b.clone().into());
+        res
+    }
+}
+
+impl<T, E> LinearLayer<T, E> for LinLayer<T, E>
+where
+    T: Tensor<E>,
+    E: Element,
+{
+}
+
+impl<T, E> LinLayer<T, E>
+where
+    T: Tensor<E>,
+    E: Element + From<f64>,
+{
+    fn new(i_size: usize, o_size: usize) -> Self {
+        let weights = T::from_vec(&vec![o_size, i_size], data)
+            .expect("Ensured data can be arranged into a matrix of the given size.");
+        let bias = T::from_vec(&vec![o_size, 1_usize], data)
+            .expect("Ensured data can be arranged into a matrix of the given size.");
+
+        LinLayer {
+            w: weights,
+            b: bias,
+            tensor_element_phantom: PhantomData,
+        }
     }
 }
 
@@ -28,9 +56,5 @@ where
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn it_works() {
-    //     let result = add(2, 2);
-    //     assert_eq!(result, 4);
-    // }
+    // #[test]s
 }
