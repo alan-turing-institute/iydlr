a0ddbdc476e34b4b88b9aa776f5436b8c17d84e0
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-06-11 16:05:53 +0100

    more work on LinLayer implementation

--- neural_nets/src/lib.rs
+++ neural_nets/src/lib.rs
@@ -1,6 +1,10 @@
 use interfaces::deep_learning::{DLModule, LinearLayer};
 use interfaces::tensors::Element;
 use interfaces::tensors::Tensor;
+use rand::distributions::Distribution;
+use rand::SeedableRng;
+use rand_chacha::ChaCha8Rng;
+use statrs::distribution::Normal;
 use std::marker::PhantomData;
 pub mod embedding_table;
 
@@ -17,6 +21,7 @@ where
 {
     type DLModuleError = <T as Tensor<E>>::TensorError;
     fn forward(&self, x: &T) -> Result<T, Self::DLModuleError> {
+        // The addition assumes broadcasting if x is a batch (b,i)
         Ok(self.w.matmul(&x.clone().transpose())? + self.b.clone())
     }
     fn params(&self) -> Vec<E> {
@@ -38,10 +43,32 @@ where
     T: Tensor<E>,
     E: Element + From<f64>,
 {
-    fn new(i_size: usize, o_size: usize) -> Self {
-        let weights = T::from_vec(&vec![o_size, i_size], data)
+    fn new(i_size: usize, o_size: usize, seed: u64) -> Self {
+        // He weight initialisation
+        // https://machinelearningmastery.com/weight-initialization-for-deep-learning-neural-networks/
+        let noise_mean = 0.0;
+        let noise_std = f64::sqrt(2.0 / (i_size as f64));
+        let rng = ChaCha8Rng::seed_from_u64(seed);
+        let normal = Normal::new(noise_mean, noise_std).unwrap();
+        let normal_itr = normal.sample_iter(rng);
+
+        let (w_data, b_data): (Vec<E>, Vec<E>) = normal_itr
+            .take(o_size * (i_size + 1))
+            .map(E::from)
+            .enumerate()
+            .fold((Vec::new(), Vec::new()), |mut acc, (idx, el)| {
+                if idx < o_size * i_size {
+                    acc.0.push(el);
+                    acc
+                } else {
+                    acc.1.push(el);
+                    acc
+                }
+            });
+
+        let weights = T::from_vec(&vec![o_size, i_size], &w_data)
             .expect("Ensured data can be arranged into a matrix of the given size.");
-        let bias = T::from_vec(&vec![o_size, 1_usize], data)
+        let bias = T::from_vec(&vec![o_size, 1_usize], &b_data)
             .expect("Ensured data can be arranged into a matrix of the given size.");
 
         LinLayer {
