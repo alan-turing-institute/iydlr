2f179b8fe33f8afa2208a5258e97002f638589e2
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-06-12 10:26:41 +0100

    Dual number tests

--- elements/src/dual_number.rs
+++ elements/src/dual_number.rs
@@ -44,6 +44,7 @@ impl AddAssign for DualNumber {
 impl Mul for DualNumber {
     type Output = Self;
     //  (ax + (ay + xb) i) =  (x + iy) * (a + bi)
+    // Problem in Mul?
     fn mul(self, rhs: Self) -> Self::Output {
         let real = self.real * rhs.real;
         let dual = self.real * rhs.dual + self.dual * rhs.real;
@@ -99,7 +100,7 @@ impl Ln for DualNumber {
 impl Pow for DualNumber {
     fn pow(self, exp: Self) -> Self {
         let real = self.real.powf(exp.real);
-        let dual = real * (exp.dual * self.real.ln() + exp.real * self.dual / self.real);
+        let dual = real * (exp.dual * self.real.ln() + (self.dual * exp.real / self.real));
         Self::new(real, dual)
     }
 }
@@ -114,6 +115,28 @@ impl Zero for DualNumber {
 }
 
 impl Element for DualNumber {}
+
+fn grad<F: Fn(DualNumber) -> DualNumber>(func: F, value: f64) -> f64 {
+    func(DualNumber::new(value, 1.)).dual
+}
+
+impl DualNumber {
+    fn grad<F: Fn(DualNumber) -> DualNumber>(func: F, value: f64) -> f64 {
+        func(DualNumber::new(value, 1.)).dual
+    }
+}
+
+pub trait Grad {
+    fn grad(&self, value: f64) -> f64;
+}
+impl<F> Grad for F
+where
+    F: Fn(DualNumber) -> DualNumber,
+{
+    fn grad(&self, value: f64) -> f64 {
+        grad(self, value)
+    }
+}
 
 impl RealElement for DualNumber {
     fn neg_inf() -> Self {
@@ -146,11 +169,39 @@ mod tests {
         dual_number.pow(DualNumber::new(3., 0.))
     }
 
+    // Expression: f(x) = 2x^2 + exp(5x)
+    //             f'(x)= 4x + 5 * exp(5x)
+    fn test_exp_fn(dual_number: DualNumber) -> DualNumber {
+        // dual_number * dual_number.pow(DualNumber::new(2., 0.)) +
+        dual_number * dual_number * dual_number
+            + DualNumber::new(f64::exp(1.), 0.).pow(DualNumber::new(5., 0.) * dual_number)
+    }
+
+    fn test_exp_fn_deriv(value: f64) -> f64 {
+        4. * value + 5. * f64::exp(1.).powf(5. * value)
+    }
+
     #[test]
     fn test_cube() {
         let dual_number = DualNumber::new(0.1, 1.);
         let result = cube(dual_number);
         assert_approx_eq!(f64, result.real, 0.001);
         assert_approx_eq!(f64, result.dual, 0.03);
+    }
+
+    #[test]
+    fn test_grad() {
+        assert_approx_eq!(f64, DualNumber::grad(cube, 0.1), 0.03);
+        assert_approx_eq!(f64, grad(cube, 0.1), 0.03);
+        assert_approx_eq!(f64, cube.grad(0.1), 0.03);
+        // assert_approx_eq!(f64, test_exp_fn.grad(3.0), 16_345_098.862_360_554_f64);
+        assert_approx_eq!(f64, test_exp_fn.grad(3.0), test_exp_fn_deriv(3.0));
+
+        let f = |x: DualNumber| x.pow(DualNumber::new(10., 0.));
+        let fp = |x: f64| 10. * x.powf(9.);
+        assert_approx_eq!(f64, f.grad(10.), fp(10.));
+        let f = |x: DualNumber| DualNumber::new(f64::exp(1.), 0.).pow(DualNumber::new(10., 0.) * x);
+        // let fp = |x: f64| 10. * f64::exp(10. * x);
+        // assert_approx_eq!(f64, f.grad(2.), fp(2.));
     }
 }
