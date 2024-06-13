6e4bf3fefea67af0288a5b43756a2f756b046149
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2024-06-13 14:13:50 +0100

    WIP: creating PELayer

--- embeddings/src/pos_encoding.rs
+++ embeddings/src/pos_encoding.rs
@@ -1,15 +1,16 @@
-use interfaces::tensors::Tensor;
+use interfaces::{deep_learning::DLModule, tensors::{Element, RealTensor, Tensor}};
 // use interfaces::tensors::TensorImpl;
 use tensors::TensorImpl;
+use std::marker::PhantomData;
 
 // pub const N: f64 = 10000.0;
 
-pub fn pos_encoding(seq_len: usize, d: usize, n: usize) -> TensorImpl<f64> {
+pub fn pos_encoding(seq_len: usize, d: usize, n: usize) -> impl Tensor<f64> {
     if d % 2 != 0 {
         panic!("The dimension d must be even");
     }
-    if d < seq_len {
-        panic!("d must not be less than seq_len");
+    if d < 4 {
+        panic!("d must be greater or equal to 4");
     }
 
     let mut accumulator = Vec::<f64>::with_capacity(d * d);
@@ -28,12 +29,47 @@ pub fn pos_encoding(seq_len: usize, d: usize, n: usize) -> TensorImpl<f64> {
         }
     }
 
-    println!("{:?}", accumulator);
-
     // Using PyTorch's behaviour as a reference
     let shape = vec![seq_len, 1, d];
-    TensorImpl::from_vec(&shape, &accumulator).unwrap()
+    Tensor::from_vec(&shape, &accumulator).unwrap()
 }
+
+
+// Create PE layer with implements DLModule
+
+pub struct PELayer<T: Tensor<E>, E: Element>{
+    tensor_phantom: PhantomData<T>,
+    tensor_element_phantom: PhantomData<E>,
+}
+
+
+impl<T, E> DLModule<T, E> for PELayer<T, E> 
+where
+    T: Tensor<E>,
+    E: Element + Into<f64>,
+{
+    type DLModuleError = <T as T<E>>::TensorError;
+
+    fn forward(&self, x: &T) -> Result<T, Self::DLModuleError> {
+        let input_shape = x.shape();
+        let seq_len = input_shape[1];
+        let d = input_shape[2];
+        let pe = pos_encoding(seq_len, d, 10000);
+        
+        Ok(x.add(pe))
+    }
+    
+
+    // This should return a empty vector
+    fn params(&self) -> Vec<f64> {
+        Vec::<f64>::new()
+    }
+    
+}
+
+
+
+
 
 #[cfg(test)]
 mod tests {
