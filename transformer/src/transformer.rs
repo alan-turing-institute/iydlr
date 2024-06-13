4baa3e61a27ee6e6298f8178829d21b43662a6ef
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-06-13 14:57:27 +0100

    WIP: transformer impl

--- transformer/src/transformer.rs
+++ transformer/src/transformer.rs
@@ -1,42 +1,83 @@
+use std::marker::PhantomData;
+
 use crate::block::Block;
+use attention::attention::SelfAttention;
 use attention::attention::{El, La, Mal, Te};
 use autodiff::node::Node;
 use config::Config;
-use interfaces::deep_learning::DLModule;
-use interfaces::tensors::RealElement;
+use interfaces::deep_learning::LinearLayer;
+use interfaces::deep_learning::{ActivationLayer, DLModule};
 use interfaces::tensors::RealTensor;
+use interfaces::tensors::{RealElement, Tensor};
 use neural_nets::optim::bce;
 use neural_nets::{act_layer::ActLayer, lin_layer::LinLayer, optim::OptimSGD, serial::Serial};
 use tensors::TensorImpl;
 
-pub struct Transformer {
-    model: Serial<Te, El>,
+// pub struct Transformer {
+//     model: Serial<Te, El>,
+// }
+
+pub struct Transformer<L, A, T, E, Al>
+where
+    L: LinearLayer<T, E>,
+    A: SelfAttention<T, E>,
+    T: Tensor<E>,
+    E: RealElement,
+    Al: ActivationLayer<T, E>,
+{
+    model: Serial<T, E>,
+    _marker_l: std::marker::PhantomData<L>,
+    _marker_a: std::marker::PhantomData<A>,
+    _marker_al: std::marker::PhantomData<Al>,
 }
 
-impl Transformer {
-    pub fn new(config: &Config) -> Transformer {
-        let mut modules: Vec<Box<dyn DLModule<Te, El>>> = (0..config.num_blocks)
+impl Transformer<La, Mal, Te, El, ActLayer<Te, El>> {
+    pub fn new(config: &Config) -> Self {
+        // let mut modules: Vec<Box<dyn DLModule<Te, El>>> = (0..config.num_blocks)
+        //     .map(|i| Box::new(Block::new(&config, i == 0)))
+        //     .collect();
+
+        let mut modules = (0..config.num_blocks)
             .map(|i| Box::new(Block::new(&config, i == 0)))
             .collect();
-        modules.push(Box::new(LinLayer::new(
-            config.embed_dim,
-            config.vocab_size,
-            config.seed,
-        )));
-        let model: Serial<T, E> = Serial::new(modules);
-        return model;
+        // modules.push(Box::new(LinLayer::new(
+        //     config.embed_dim,
+        //     config.vocab_size,
+        //     config.seed,
+        // )));
+        // let model: Serial<Te, El> = Serial::new(modules);
+        let model: Serial<Te, El> = Serial::new(
+            (0..config.num_blocks)
+                .map(|i| Box::new(Block::new(&config, i == 0)))
+                .collect::<Vec<_>>(),
+        );
+        Self {
+            model,
+            _marker_a: PhantomData,
+            _marker_al: PhantomData,
+            _marker_l: PhantomData,
+        }
     }
 }
 
-impl<T, E, L, A, Al> DLModule<T, E> for Transformer {
-    type DLModuleError = Te::TensorError;
+impl<T, E, L, A, Al> DLModule<T, E> for Transformer<T, E, L, A, Al>
+where
+    L: LinearLayer<T, E>,
+    A: SelfAttention<T, E>,
+    T: Tensor<E>,
+    E: RealElement,
+    Al: ActivationLayer<T, E>,
+{
+    type DLModuleError = <T as Tensor<E>>::TensorError;
 
-    fn forward(&self, x: &Te) -> Result<Te, Self::DLModuleError> {
-        return self.model.forward(x);
+    fn forward(&self, x: &T) -> Result<T, Self::DLModuleError> {
+        // return self.model.forward(x);
+        todo!()
     }
 
-    fn params(&self) -> Vec<El> {
-        return self.model.params();
+    fn params(&self) -> Vec<E> {
+        // return self.model.params();
+        todo!()
     }
 }
 
