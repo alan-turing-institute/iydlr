6dac3373d824bae894b2487500189d6e621eec2b
Cyara-she-her <cbuchuck-wilsenach@turing.ac.uk>
2024-06-13 14:29:40 +0100

    Co-authored-by: Ed Chapman <edchapman88@users.noreply.github.com>

--- tokenisation/src/lib.rs
+++ tokenisation/src/lib.rs
@@ -1,11 +1,61 @@
 //! The text I am linking below is Louisa May Alcotts Little Women and it will serve as our dataset to run the model on
 //! tokenisation] (.../mytext.txt)
-use std::collections::btree_map::Values;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs::read_to_string;
-use std::ops::Index;
-use std::{char, fs, mem::take, string};
+use std::{char, fs};
+pub struct Tokeniser {
+    char_to_index: HashMap<char, usize>,
+    index_to_char: HashMap<usize, char>,
+}
+
+impl Tokeniser {
+    pub fn new(text: &str) -> Tokeniser {
+        // find unique characters in text
+        let mut unique_chars = HashSet::new();
+        for c in text.chars() {
+            unique_chars.insert(c);
+        }
+        // Build character to index Hash map
+        let mut char_to_index: HashMap<char, usize> = HashMap::new();
+        let mut index = 0;
+        for c in unique_chars {
+            char_to_index.insert(c, index);
+            index += 1;
+        }
+        // Build index to character hash map
+        let mut index_to_char: HashMap<usize, char> = HashMap::new();
+        for (c, idx) in &char_to_index {
+            index_to_char.insert(*idx, *c);
+        }
+
+        Tokeniser {
+            char_to_index,
+            index_to_char,
+        }
+    }
+
+    pub fn encode(&self, text: &str) -> Vec<usize> {
+        let mut result = Vec::new();
+        for c in text.chars() {
+            let idx = self
+                .char_to_index
+                .get(&c)
+                .expect("character not found in tokeniser!");
+            result.push(*idx);
+        }
+        result
+    }
+
+    pub fn decode(&self, indices: &[usize]) -> String {
+        let mut result = String::new();
+        for idx in indices {
+            let char = self.index_to_char.get(&idx).unwrap();
+            result.push(*char);
+        }
+        result
+    }
+}
 
 pub fn read_txt(path: &str) -> String {
     fs::read_to_string(path).unwrap()
@@ -24,9 +74,9 @@ pub fn main() {
         '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ' ',
     ];
     let first_thou: String = story.chars().take(1000).collect();
-    println!("{:?}", story.chars().take(10).collect::<Vec<_>>());
+    // println!("{:?}", story.chars().take(10).collect::<Vec<_>>());
 
-    println!("Number of characers: {}", story.chars().count());
+    println!("Number of characters: {}", story.chars().count());
 
     let file_path = "my_text.txt";
     let var_name = match read_to_string(file_path) {
@@ -74,23 +124,21 @@ pub fn main() {
     //Index to Character Hashmap, which maps index numbers to characters starting from 0
     println!("Index to Character HashMap:");
     let mut index_to_char: HashMap<i32, char> = HashMap::new();
-    for (char, Index) in &char_to_index {
-        index_to_char.insert(index, *char);
-        index += 1;
+    for (char, idx) in &char_to_index {
+        index_to_char.insert(*idx, *char);
     }
-        let mut indices: Vec<_> = index_to_char.keys().collect();
-        indices.sort();
-        for index in indices {
-            if let Some(char)=index_to_char.get(index){
-                println!("Index: {}, Character: {}", index, char);
-            }
+    let mut indices: Vec<_> = index_to_char.keys().collect();
+    indices.sort();
+    for index in indices {
+        if let Some(char) = index_to_char.get(index) {
+            println!("Index: {}, Character: {}", index, char);
         }
     }
-    // println!("Index to Character HashMap:");
-    // index_to_char.iter().for_each(|(index, char)| {
-    //     println!("Index: {}, Character: {}", index, char);
-    // });
-
+}
+// println!("Index to Character HashMap:");
+// index_to_char.iter().for_each(|(index, char)| {
+//     println!("Index: {}, Character: {}", index, char);
+// });
 
 #[cfg(test)]
 mod tests {
@@ -99,5 +147,23 @@ mod tests {
     #[test]
     fn hello_world() {
         main()
+    }
+
+    #[test]
+    fn tokeniser_encode() {
+        let text = "The quick brown dog.";
+        let tokeniser = Tokeniser::new(text);
+        let tokens = tokeniser.encode("wow The brow on The dog");
+
+        println!("{:?}", tokens);
+    }
+
+    #[test]
+    fn tokeniser_decode() {
+        let text = "The quick brown dog.";
+        let tokeniser = Tokeniser::new(text);
+        let letters = tokeniser.decode(&vec![3, 0, 0, 2]);
+
+        println!("{:?}", letters);
     }
 }
