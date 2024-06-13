61a610370085d367599b3a6d3c09c9dcb8d959a5
Tim Hobson <thobson88@gmail.com>
2024-06-13 12:25:35 +0100

    Add positional encoding fn

--- embeddings/src/lib.rs
+++ embeddings/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod pos_encoding;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
