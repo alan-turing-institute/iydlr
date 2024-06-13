0b79c40760b272a3c41725ba3f007927e1c8596d
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-06-13 16:21:52 +0100

    categorical cross entropy loss function
    Co-authored-by: Tim Hobson <thobson88@users.noreply.github.com>
    Co-authored-by: Andy Smith <andrewphilipsmith@users.noreply.github.com>

--- neural_nets/src/optim.rs
+++ neural_nets/src/optim.rs
@@ -59,3 +59,14 @@ where
             + (t_ones.clone() + (y * E::from(-1.0)))
                 * (t_ones + (y_pred + E::from(-0.0000001)) * E::from(-1.0)).ln())
 }
+
+/// Categorical (i.e. multi-label) cross entropy loss function.
+pub fn cce<T, E>(y: T, y_pred: T) -> T
+where
+    T: RealTensor<E>,
+    E: RealElement + From<f64>,
+{
+    let y_shape = y.shape();
+    let t_ones = T::fill_with_clone(vec![y_shape[0], 1, y_shape[2]], E::from(1.0));
+    (y * y_pred.ln()).matmul(&t_ones.transpose()).unwrap() // TODO: minus 1 *
+}
