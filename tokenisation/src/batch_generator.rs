332cab763a5b8c8a0074e47af951ceb20c682cb6
Cyara-she-her <cbuchuck-wilsenach@turing.ac.uk>
2024-06-13 15:31:45 +0100

    Finished tokeniser and began batch generator

---
+++ tokenisation/src/batch_generator.rs
@@ -0,0 +1,66 @@
+use crate::tokeniser::Tokeniser;
+use rand::Rng;
+use rand::SeedableRng;
+use rand_chacha::ChaCha8Rng;
+
+// Making a batch generator
+pub struct BatchGenerator {
+    rng: ChaCha8Rng,
+    text: String,
+    tokeniser: Tokeniser,
+    tokens: Vec<usize>,
+    chunk_len: usize,
+    batch_size: usize,
+}
+
+impl BatchGenerator {
+    pub fn new(text: String, chunk_len: usize, batch_size: usize, seed: u64) -> BatchGenerator {
+        let rng = ChaCha8Rng::seed_from_u64(seed);
+        let tokeniser = Tokeniser::new(&text);
+        let tokens = tokeniser.encode(&text);
+        BatchGenerator {
+            rng,
+            text,
+            tokeniser,
+            tokens,
+            chunk_len,
+            batch_size,
+        }
+    }
+
+    pub fn sample(&mut self) -> TrainingExample {
+        let rand_idx = self
+            .rng
+            .gen_range(0..self.tokens.len() - (self.chunk_len + 1));
+        let input = self.tokens[rand_idx..(rand_idx + self.chunk_len)].to_vec();
+        let target = self.tokens[(rand_idx + 1)..(rand_idx + self.chunk_len + 1)].to_vec();
+
+        TrainingExample { input, target }
+    }
+}
+
+pub struct TrainingExample {
+    pub input: Vec<usize>,
+    pub target: Vec<usize>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hello_world() {
+        let seed = 0;
+        let text = "this is some dummy text".into();
+        let chunk_len = 4;
+        let batch_size = 1;
+        let mut batch_gen = BatchGenerator::new(text, chunk_len, batch_size, seed);
+
+        let example = batch_gen.sample();
+        println!("input: {:?}", example.input);
+        println!("target: {:?}", example.target);
+        assert_eq!(example.input[1], example.target[0]);
+        assert_eq!(example.input[2], example.target[1]);
+        assert_eq!(example.input[3], example.target[2]);
+    }
+}
--- tokenisation/src/lib.rs
+++ tokenisation/src/lib.rs
@@ -4,58 +4,8 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs::read_to_string;
 use std::{char, fs};
-pub struct Tokeniser {
-    char_to_index: HashMap<char, usize>,
-    index_to_char: HashMap<usize, char>,
-}
-
-impl Tokeniser {
-    pub fn new(text: &str) -> Tokeniser {
-        // find unique characters in text
-        let mut unique_chars = HashSet::new();
-        for c in text.chars() {
-            unique_chars.insert(c);
-        }
-        // Build character to index Hash map
-        let mut char_to_index: HashMap<char, usize> = HashMap::new();
-        let mut index = 0;
-        for c in unique_chars {
-            char_to_index.insert(c, index);
-            index += 1;
-        }
-        // Build index to character hash map
-        let mut index_to_char: HashMap<usize, char> = HashMap::new();
-        for (c, idx) in &char_to_index {
-            index_to_char.insert(*idx, *c);
-        }
-
-        Tokeniser {
-            char_to_index,
-            index_to_char,
-        }
-    }
-
-    pub fn encode(&self, text: &str) -> Vec<usize> {
-        let mut result = Vec::new();
-        for c in text.chars() {
-            let idx = self
-                .char_to_index
-                .get(&c)
-                .expect("character not found in tokeniser!");
-            result.push(*idx);
-        }
-        result
-    }
-
-    pub fn decode(&self, indices: &[usize]) -> String {
-        let mut result = String::new();
-        for idx in indices {
-            let char = self.index_to_char.get(&idx).unwrap();
-            result.push(*char);
-        }
-        result
-    }
-}
+pub mod batch_generator;
+pub mod tokeniser;
 
 pub fn read_txt(path: &str) -> String {
     fs::read_to_string(path).unwrap()
@@ -147,23 +97,5 @@ mod tests {
     #[test]
     fn hello_world() {
         main()
-    }
-
-    #[test]
-    fn tokeniser_encode() {
-        let text = "The quick brown dog.";
-        let tokeniser = Tokeniser::new(text);
-        let tokens = tokeniser.encode("wow The brow on The dog");
-
-        println!("{:?}", tokens);
-    }
-
-    #[test]
-    fn tokeniser_decode() {
-        let text = "The quick brown dog.";
-        let tokeniser = Tokeniser::new(text);
-        let letters = tokeniser.decode(&vec![3, 0, 0, 2]);
-
-        println!("{:?}", letters);
     }
 }
